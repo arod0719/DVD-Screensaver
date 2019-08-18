0c043b55025368031873a96b30c031c8dd9efa8e
Alex Rodriguez <50396589+arod0719@users.noreply.github.com>
2019-08-18 04:41:26 -0500

    Update dvdlogo.py

--- dvdlogo.py
+++ dvdlogo.py
@@ -4,33 +4,33 @@ import random
 length = 884
 height = 746
 
-turtle.setup(length, height)
+turtle.setup(length, height) #used to create screen dimensions
 wn = turtle.Screen()
 turtle.tracer(0, 0)
-wn.title("DVD")
-wn.bgcolor("black")
+wn.title("DVD") #Title of application
+wn.bgcolor("black") #Background color
 ball = turtle.Turtle()
-ball.shape("square")
-ball.color("green")
+ball.shape("square") #Shape of bouncing device
+ball.color("green") #Start color as green
 ball.speed(.1)
 ball.dx = 0
-ball.penup()
-random_options = [.98,.99,1,1.01,1.02,1.03]
-color_list = ["red", "orange", "yellow", "green", "blue", "purple", "cyan", "pink"]
+ball.penup() #Commenting this line allows you to view where the logo has gone
+random_options = [.98,.99,1,1.01,1.02,1.03] #Random multipliers to ensure logo doesn't fall into a loop
+color_list = ["red", "orange", "yellow", "green", "blue", "purple", "cyan", "pink"] #list of colors the logo will change to during collision
 x = 1
 y = 1
 
-wallCount = 0
-cornerCount = 0
+wallCount = 0 #wall count hits
+cornerCount = 0 #corner counts
 
-wordsHits = turtle.Turtle()
+wordsHits = turtle.Turtle() #Uses a turtle inorder to show corner counts within the application
 wordsHits.goto((length/2)*-1 + 30,(height/2)*-1 +100)
 wordsHits.color("green")
 wordsHits.hideturtle()
 wordsHits.write("Corner Hits: {}".format(cornerCount), False)
 
 
-wordsMiss = turtle.Turtle()
+wordsMiss = turtle.Turtle() #Uses a turtle inorder to show wall counts within the application
 wordsMiss.goto((length/2)*-1 + 30,(height/2)*-1 +85)
 wordsMiss.color("green")
 wordsMiss.hideturtle()
@@ -44,14 +44,14 @@ difficulty = 20
 
 
 
-def testcorner(x,y):
+def testcorner(x,y): #function used to determine if a corner was hit or not
     global cornerCount
     global wallCount
-    colorTemp = random.choice(color_list)
+    colorTemp = random.choice(color_list) #randomizes the color
     ball.color(colorTemp)
     wordsMiss.color(colorTemp)
     wordsHits.color(colorTemp)
-    if abs((x-((length/2)*-1 + 5))) <= difficulty and abs((y - (height/2)*-1)+10) <= difficulty:
+    if abs((x-((length/2)*-1 + 5))) <= difficulty and abs((y - (height/2)*-1)+10) <= difficulty: #each elif checks if a corner was hit
         cornerCount = cornerCount + 1
         wordsHits.clear()
         wordsHits.write("Corner Hits: {}".format(cornerCount), False)
